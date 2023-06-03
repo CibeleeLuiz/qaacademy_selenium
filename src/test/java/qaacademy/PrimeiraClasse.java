cf293b68718f7ad7864b855817b6d6167bfcbbd3
cibeleeLluiz <josemartins9@yahoo.com.br>
2023-06-03 16:27:55 -0300

    Jekins

--- src/test/java/qaacademy/PrimeiraClasse.java
+++ src/test/java/qaacademy/PrimeiraClasse.java
@@ -7,39 +7,30 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+
 public class PrimeiraClasse {
 
-    String cursosPresenciaisBotao = "comp-k7g9c7sd1label";
-    int tempoEspera = 2000; //2 segundos em milissegundos;
-    String cursosOnlineBotao = "comp-k7g9c7sd2label";
-    int tempoEspera1 = 2000; //2 segundos em milissegundos;
-    String contatoBotao = "//*[@id='comp-k7g9c7sd3label']";
-    int tempoEspera2 = 2000; //2 segundos em milissegundos;
- 
-    
+    String cursosPresenciais = "//*[@id='comp-k7g9c7sd1label']";
+    String cursosOnline = "//*[@id='comp-k7g9c7sd2label']";
+    String contato = "//*[@id='comp-k7g9c7sd3label']";
+    String Nome = "//*[@id='input_comp-k37gdip8']";
 
-    /**
-     * @throws InterruptedException
-     * 
-     */
+
+
     @Test
-    public void testeAberturaNvegador() throws InterruptedException{
-        WebDriver drive = new ChromeDriver();
-        drive.get("https://www.qaacademy.com.br/");
+    public void testeAberturaNavegador() throws InterruptedException{
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.qaacademy.com.br/");
         
-
-        //drive.findElement(By.id(cursosPresenciaisBotao)).click();
-        //Thread.sleep(tempoEspera);
-        //drive.findElement(By.id(cursosOnlineBotao)).click();
-        //Thread.sleep(tempoEspera1);
-        drive.findElement(By.xpath(contatoBotao)).click();
-        Thread.sleep(tempoEspera2);
-        Driver.findElement(By.id(nomecampo)).send
-        //drive.findElement(By.id(Depoimento)).click();
-        //Thread.sleep(tempoEspera3);
-        //Thread.sleep(tempoEspera4);
-        //drive.findElement(By.xpath(Matricula)).click(); 
-        
+        Thread.sleep(2000);
+        driver.findElement(By.xpath(cursosPresenciais)).click();
+        Thread.sleep(2000);
+        driver.findElement(By.xpath(cursosOnline)).click();
+        Thread.sleep(2000);
+        driver.findElement(By.xpath(contato)).click();
+        Thread.sleep(2000);
+        driver.findElement(By.xpath(Nome)).click();
+        driver.findElement(By.xpath(Nome)).sendKeys("José Luiz da Silva Martins");
 
 
     }
